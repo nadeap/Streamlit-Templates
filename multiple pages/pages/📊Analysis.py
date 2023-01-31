4ec864601469f22b38638b7f0cd794cb7ed7af9b
wertax0 <nadeaaap@gmail.com>
2023-01-31 23:21:19 +0700

    add img

--- multiple pages/pages/📊Analysis.py
+++ multiple pages/pages/📊Analysis.py
@@ -4,3 +4,6 @@ st.title("Data Analyst")
 
 st.markdown("# Welcome to my Streamlit app!")
 st.write("Analisis Data XXX")
+
+image = Image.open('img/coba.png')
+st.image(image, width=700, caption='Halo Tes Satu Dua Tiga')
